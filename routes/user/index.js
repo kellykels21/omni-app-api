75f252125cd6a637d9fe0a2fb426134a3bba8073
Markel Spellman <Kels@Users-MacBook-Pro.local>
2019-07-03 16:20:27 -0400

    added get user by auth id

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,9 +1,11 @@
 const user = require('express').Router()
 const create = require('./create')
+const getUserByAuthID = require('./getUserByAuthId')
 
 user.get('/', (req, res) =>{
   res.send(200, {message: 'User Connected.'})
 })
-
 user.post('/new', create)
+user.get('/:authID', getUserByAuthID)
+
 module.exports = user
