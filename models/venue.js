40da7aed051de419a3ded2052d646c0da71a8bf3
Markel Spellman <markel.spellman@gmail.com>
2020-08-03 22:50:06 -0400

    reworking: gutting venues schema added todos

--- models/venue.js
+++ models/venue.js
@@ -1,27 +1,9 @@
 const mongoose = require("mongoose");
 
-const pointSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    enum: ["Point"],
-    required: true,
-  },
-  coordinates: {
-    type: [Number],
-    required: true,
-  },
-});
-
 var venueSchema = new mongoose.Schema({
-  name: { type: String, required: true },
   placeId: { type: String, required: true, unique: true },
-  rsvps: [String],
-  status: [Number],
-  location: {
-    type: pointSchema,
-    required: true,
-  },
-  photoReference: { type: String, required: true },
+  attendees: [String],
+  currentHotnessRating: Number,
 });
 
 const Venue = mongoose.model("Venue", venueSchema);
--- routes/venue/index.js
+++ routes/venue/index.js
@@ -3,6 +3,10 @@ const create = require("./create");
 const getVenuesByRadius = require("./getVenuesByRadius");
 const getVenueByPlaceId = require("./getVenueByPlaceId");
 
+// TODO: update venues routes to create new venues in db
+// - handle duplicate place ids
+// - handle creations in batch
+
 venue.get("/healthcheck", (req, res) => {
   res.send(200, { message: "Venue Connected." });
 });
